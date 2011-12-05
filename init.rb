ee67ce7ee02ce8f25d407320ebc1c7921ead2e52
Aaron VonderHaar <gruen0aermel@gmail.com>
2011-12-05 11:21:12 -0800

    Require template files to have a .liquid extension

--- init.rb
+++ init.rb
@@ -45,20 +45,29 @@ Find.find(".") do |path|
   else
     name = CGI::unescape(path)
     name = Liquid::Template.parse(name).render properties
-    puts "Processing #{name}..."
-    # read the file
-    template = File.open(path, "r").read
-    # process the file
-    rendering = Liquid::Template.parse(template).render properties
-    # write the file
+    # Make the directory for the file
     dir = File.dirname(name)
     if !File.exists? dir then
       puts "Creating #{dir}"
       FileUtils.mkdir_p dir
     end
-    File.open(name, "w").write(rendering)
-    if name != path then
+    if name =~ /\.liquid$/ then
+      name = name.sub(/\.liquid$/, '')
+      puts "Processing #{name}..."
+      # read the file
+      template = File.open(path, "r").read
+      # process the file
+      rendering = Liquid::Template.parse(template).render properties
+      # write the file
+      File.open(name, "w").write(rendering)
+      # Delete the template file
       File.delete path
+    elsif name == path then
+      puts "Copying #{name}..."
+      # do nothing - already in place
+    else
+      puts "Copying #{name}..."
+      FileUtils.mv(path, name)
     end
   end
 end
