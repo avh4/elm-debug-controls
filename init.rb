873d806d666280c135d00a69be3ca87907099120
Aaron VonderHaar <gruen0aermel@gmail.com>
2011-12-05 03:14:14 -0800

    More flexible templating system that can process filenames

--- init.rb
+++ init.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 require 'liquid'
 require 'yaml'
+require 'fileutils'
+require 'find'
 
 # Open the properties file
 properties_file = File.open "properties.yaml"
@@ -18,16 +20,40 @@ end
 # Load the properties
 properties = YAML::load(properties_yaml)
 
+# Create Liquid filters
+module NameFilter
+  def as_path(input)
+    input.tr('.', '/').tr('-', '')
+  end
+end
+Liquid::Template.register_filter(NameFilter)
+
 # process the files
-files = [ "pom.xml" ]
-files.each do |file|
-  puts "Processing #{file}..."
-  # read the file
-  template = File.open(file, "r").read
-  # process the file
-  rendering = Liquid::Template.parse(template).render properties
-  # write the file
-  File.open(file, "w").write(rendering)
+ignore = [ ".git" ]
+Find.find(".") do |path|
+  if ignore.include? File.basename(path) then
+    Find.prune
+  elsif FileTest.directory?(path)
+    next
+  else
+    name = CGI::unescape(path)
+    name = Liquid::Template.parse(name).render properties
+    puts "Processing #{name}..."
+    # read the file
+    template = File.open(path, "r").read
+    # process the file
+    rendering = Liquid::Template.parse(template).render properties
+    # write the file
+    dir = File.dirname(name)
+    if !File.exists? dir then
+      puts "Creating #{dir}"
+      FileUtils.mkdir_p dir
+    end
+    File.open(name, "w").write(rendering)
+    if name != path then
+      File.delete path
+    end
+  end
 end
 
 puts "Successful.  Cleaning up..."
