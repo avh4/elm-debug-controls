714c4324742c495d241648c11b485d5b80dbab7d
Tessa Kelly <tessakkelly@gmail.com>
2019-09-10 12:14:08 -0700

    Adds longString Control

--- tests/Controls/StringTest.elm
+++ tests/Controls/StringTest.elm
@@ -9,22 +9,33 @@ import Test.Html.Query as Query
 import Test.Html.Selector exposing (..)
 
 
-stringControls =
-    Control.string "default"
-
-
 all : Test
 all =
-    describe "Control.string"
-        [ test "initial value" <|
-            \() ->
-                stringControls
-                    |> Control.currentValue
-                    |> Expect.equal "default"
-        , test "Renders all options" <|
-            \() ->
-                stringControls
-                    |> Control.view identity
-                    |> Query.fromHtml
-                    |> Query.has [ tag "input", attribute (Html.value "default") ]
+    describe "Text"
+        [ describe "Control.string"
+            [ test "initial value" <|
+                \() ->
+                    Control.string "default"
+                        |> Control.currentValue
+                        |> Expect.equal "default"
+            , test "Renders all options" <|
+                \() ->
+                    Control.string "default"
+                        |> Control.view identity
+                        |> Query.fromHtml
+                        |> Query.has [ tag "input", attribute (Html.value "default") ]
+            ]
+        , describe "Control.longString"
+            [ test "initial value" <|
+                \() ->
+                    Control.longString "long default"
+                        |> Control.currentValue
+                        |> Expect.equal "long default"
+            , test "Renders all options" <|
+                \() ->
+                    Control.longString "long default"
+                        |> Control.view identity
+                        |> Query.fromHtml
+                        |> Query.has [ tag "textarea", attribute (Html.value "long default") ]
+            ]
         ]
